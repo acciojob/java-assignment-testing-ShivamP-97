07b26df78e05947d834f10161f22b1b7e4fd43a3
= <shivampathak19972@gmail.com>
2026-01-15 22:37:35 +0530

    Test

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,64 +1,8 @@
 package com.driver.test;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import Main;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
 public class TestCases {
-
-    private final PrintStream originalOut = System.out;
-    private final java.io.InputStream originalIn = System.in;
-    private ByteArrayOutputStream outContent;
-
-    @Before
-    public void setUp() {
-        outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @After
-    public void tearDown() {
-        System.setOut(originalOut);
-        System.setIn(originalIn);
-    }
-
-    private void provideInput(String data) {
-        System.setIn(new ByteArrayInputStream(data.getBytes()));
-    }
-
-    @Test
-    public void testNEqualsOne() {
-        provideInput("1");
-        Main.main(new String[]{});
-        assertEquals("a\n", outContent.toString());
-    }
-
-    @Test
-    public void testNEqualsTwo() {
-        provideInput("2");
-        Main.main(new String[]{});
-        assertEquals("ab\n", outContent.toString());
-    }
-
-    @Test
-    public void testNEqualsThree() {
-        provideInput("3");
-        Main.main(new String[]{});
-        assertEquals("cab\n", outContent.toString());
-    }
-
-    @Test
-    public void testZeroInput() {
-        provideInput("0");
-        Main.main(new String[]{});
-        assertEquals("\n", outContent.toString());
-    }
+    
 }
